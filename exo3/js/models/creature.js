e3c19f997c1286fdab37725bbf1ff2e44bd3d15c
Nicolas Dupont <nicolas@akeneo.com>
2014-05-02 20:38:26 +0200

    Update models to describe the map and allow creatures moving

--- exo3/js/models/creature.js
+++ exo3/js/models/creature.js
@@ -10,9 +10,10 @@ $(function(){
             name: 'My creature',
             hp: 100,
             maxHp: 100,
-            checked: false,
-            posX: 0,
-            posY: 0
+            posX: 1,
+            posY: 1,
+            direction: null,
+            checked: false
         },
         toggle: function(){
             this.set('checked', !this.get('checked'));
@@ -23,13 +24,61 @@ $(function(){
         isDead: function(){
             return this.get('hp') <= 0;
         },
-        act: function(){
-            var limit = 12;
-            if (this.get('posX') < limit) {
-                this.set('posX', this.get('posX') + 1);
-            } else {
-                this.set('posX', this.get('posX') - 1);
+        hasDirection: function(){
+            return this.get('direction') !== null;
+        },
+        getCompass: function(){
+            return {
+                'N': {'top': -1, 'left':0},
+                'NE': {'top': -1, 'left':+1},
+                'E': {'top': 0, 'left':+1},
+                'SE': {'top': +1, 'left':+1},
+                'S': {'top': +1, 'left':0},
+                'SW': {'top': +1, 'left':-1},
+                'W': {'top': 0, 'left':-1},
+                'NW': {'top': -1, 'left':-1}
+            };
+        },
+        getAvailableDirections: function(){
+            var directions = this.getCompass();
+            var availableDirections = {};
+            for (var code in directions) {
+                var x = this.get('posX') + directions[code]['left'];
+                var y = this.get('posY') + directions[code]['top'];
+                if (x >= 0 && y >= 0 && app.game.isAvailableTile(x, y) === true) {
+                    availableDirections[code]= directions[code];
+                }
             }
+            return availableDirections;
+        },
+        chooseDirection: function(){
+            var directions = this.getAvailableDirections();
+            var nb = Object.keys(directions).length;
+            if (nb > 0) {
+                var idx = Math.floor((Math.random() * nb));
+                this.set('direction', Object.keys(directions)[idx]);
+            } else {
+                this.set('direction', null);
+            }
+        },
+        canMove: function(){
+            var direction = this.get('direction');
+            var available = this.getAvailableDirections();
+            return available.hasOwnProperty(direction);
+        },
+        move: function(){
+            if (this.hasDirection() === false || this.canMove() === false) {
+                this.chooseDirection();
+            }
+            if (this.canMove() === true) {
+                var direction = this.getCompass()[this.get('direction')];
+                this.set('posX', this.get('posX') + direction.left);
+                this.set('posY', this.get('posY') + direction.top);
+            }
+        },
+        act: function(){
+            this.move();
+            this.set('hp', this.get('hp')-5);
         }
     });
 });
--- exo3/js/models/game.js
+++ exo3/js/models/game.js
@@ -6,14 +6,90 @@ $(function(){
 
     // Game model
     app.Game = Backbone.Model.extend({
-        nextTurn: function(){
+        defaults:{
+            tiles: [],
+            tileSize: 20,
+            maxTileX: 0,
+            maxTileY: 0,
+            treeChar: '#',
+            rockChar: 'o',
+            snowChar: ' ',
+            fps: 3,
+            intervalId: null
+        },
+        initialize: function(){
+            this.set('tiles', [
+                "####################################",
+                "#      #    #      ##             ##",
+                "#                       ##         #",
+                "#          #####        ###        #",
+                "##    o    #   #    ##             #",
+                "###           ##     #             #",
+                "#           ###      #             #",
+                "#   ###                            #",
+                "#    ###                     o     #",
+                "#   ####       ##           oo     #",
+                "#   ###         ####               #",
+                "#                ###               #",
+                "#   ####          ###              #",
+                "#   ####                           #",
+                "#   ##                             #",
+                "#                     ooo          #",
+                "#                    ooooo         #",
+                "#                     ooo          #",
+                "#                                  #",
+                "#                                  #",
+                "#          ######                  #",
+                "#           ####           oo      #",
+                "#            ##             o      #",
+                "#o                 ##              #",
+                "#                 ####             #",
+                "#                  ####            #",
+                "#                     ##           #",
+                "#                      #           #",
+                "#    #             #           ### #",
+                "#    #            ##               #",
+                "####################################"]);
+            this.set('maxTileX', this.get('tiles')[0].length);
+            this.set('maxTileY', this.get('tiles').length);
+        },
+        start: function(){
+            this.set('intervalId', setInterval(this.run, 1000 / this.get('fps')));
+            return this;
+        },
+        stop: function(){
+            clearInterval(this.get('intervalId'));
+            return this;
+        },
+        run: function(){
+            var hasOneAlive = false;
             app.creatures.each(function(creature){
                 if (!creature.isDead()) {
+                    hasOneAlive = true;
                     creature.act();
                 }
             });
-            return this;
-        }
 
+            if (!hasOneAlive) {
+                this.stop();
+            }
+
+            return this;
+        },
+        isAvailableTile: function(x, y){
+            var tiles = this.get('tiles');
+            var tile = tiles[y][x];
+            if (tile === this.get('treeChar') || tile === this.get('rockChar')) {
+                return false;
+            }
+            var isAvailable = true;
+            app.creatures.each(function(creature){
+                if (creature.get('posX') === x && creature.get('posY') === y) {
+                    isAvailable = false;
+                }
+            });
+
+            return isAvailable;
+        }
     });
 });
